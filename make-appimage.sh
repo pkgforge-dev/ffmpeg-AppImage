862b75722567fc3d3215ebd2092a9f283b918946
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-30 13:58:57 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,22 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q ffmpeg | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=https://github.com/FFmpeg.png
+export DESKTOP=DUMMY
+export MAIN_BIN=ffmpeg
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun \
+	/usr/bin/ffmpeg       \
+	/usr/bin/ffplay       \
+	/usr/bin/ffprobe      \
+	/usr/bin/qt-faststart \
+	/usr/share/ffmpeg
 
 # Additional changes can be done in between here
 
